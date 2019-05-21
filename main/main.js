307e1f57e168b9781183c9bc672d14406e86c2a4
zhuyuezy <2595223212@qq.com>
2019-05-21 16:32:05 +0800

    1

--- main/main.js
+++ main/main.js
@@ -1,4 +1,11 @@
 // Write your cade below:
-module.exports = function main() {
+module.exports = function main(dividend, divisor) {
+	if (divisor==0)
+	{
+		console.log("0 should not be divisor.");
+		return;
+	}
+	var result = dividend % divisor;
+	return result;
   
 };
